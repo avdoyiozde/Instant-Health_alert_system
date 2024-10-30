5f1f17f961781ce7db7aa8d537e7272b0d91b533
Akshay v Doyizode <akshayvdoizode123@gmail.com>
2024-10-30 22:02:45 +0530

    Update consume_alert.py

--- consume_alert.py
+++ consume_alert.py
@@ -6,8 +6,8 @@ from botocore.exceptions import ClientError
 
 # Kafka consumer configuration
 consumer = KafkaConsumer(
-    'Alerts_Message',
-    bootstrap_servers=['ec2-44-196-94-216.compute-1.amazonaws.com:9092'],
+    'alert_message',
+    bootstrap_servers=['localhost:9092'],
     auto_offset_reset='earliest',
     value_deserializer=lambda m: m.decode('utf-8')
 )
@@ -15,9 +15,9 @@ consumer = KafkaConsumer(
 # Initialize SNS client
 sns_client = boto3.client('sns', region_name='us-east-1')
 sns_topic_arn = 'arn:aws:sns:us-east-1:284159339388:health-care-notification-system'
-messages=["abc","pqr"]
+
 # Publish messages to SNS topic
-for message in messages:
+for message in consumer:
     try:
         message_obj = json.loads(message.value)
         subject_header = f"Health Alert: Patient- {message_obj.get('patientname', 'Unknown')}"
